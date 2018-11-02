a6b49afd671bc5e9b1f09a2ca4b4ac900013dcd2
Sergey Zinchenko <s.zinchenko@e-mba.ru>
2018-11-02 14:26:43 +0300

    Set id in transit process

--- src/Models/ProcessOptions/TransitProcessOptions.php
+++ src/Models/ProcessOptions/TransitProcessOptions.php
@@ -11,5 +11,6 @@ class TransitProcessOptions extends ProcessOptions
             'mapping'      => [],
             'options'      => [],
         ]);
+        $this->setAttribute('id', $systemId);
     }
 }
